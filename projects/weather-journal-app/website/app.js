4506791e051697b0fbdfa0d209fac5bed914bc40
Aditya Matsa <aditya.matsa@icloud.com>
2024-05-21 13:06:46 +0530

    Setup a GET method for querying website

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -7,4 +7,19 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 const apiEndPoint = 'https://api.openweathermap.org/geo/1.0/zip?zip='
 const zipCode = document.getElementById('zip').value;
 const apiKey = '&appid=aac1d7f7625ee92bc0a51d78900a24a7';
-const urlPlace = apiEndPoint + zipCode + apiKey;
+
+async function getLatLong (data) {
+    const response = await fetch(apiEndPoint + zipCode + apiKey, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+}

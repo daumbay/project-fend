8f22f394000a275c4abd258377234dcc9836dfa6
Aditya Matsa <aditya.matsa@icloud.com>
2024-05-21 12:50:15 +0530

    Setup global variables in app.js

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -3,3 +3,8 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const apiEndPoint = 'https://api.openweathermap.org/geo/1.0/zip?zip='
+const zipCode = document.getElementById('zip').value;
+const apiKey = '&appid=aac1d7f7625ee92bc0a51d78900a24a7';
+const urlPlace = apiEndPoint + zipCode + apiKey;

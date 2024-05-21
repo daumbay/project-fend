cdbe69e0dfb95b6250af9c58c8d30b847a482827
Aditya Matsa <aditya.matsa@icloud.com>
2024-05-21 14:30:27 +0530

    Resolve CORS issue

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -4,17 +4,11 @@
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-const apiEndPoint = 'https://api.openweathermap.org/geo/1.0/zip?zip='
-const zipCode = document.getElementById('zip').value;
+const apiEndPoint = 'https://api.openweathermap.org/geo/1.0/zip?zip=';
 const apiKey = '&appid=aac1d7f7625ee92bc0a51d78900a24a7';
 
-async function getLatLong (url = '', data = {}) {
-    const response = await fetch(url, {
-        method: 'GET',
-        credentials: 'same-origin',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    });
+async function getLatLong (url = '') {
+    const response = await fetch(url);
 
     try {
         const newData = await response.json();
@@ -25,5 +19,7 @@ async function getLatLong (url = '', data = {}) {
 }
 
 document.getElementById('generate').addEventListener('click', () => {
-    const response = getLatLong(apiEndPoint + zipCode + apiKey, {});
+    const zipCode = document.getElementById('zip').value;
+    const response = getLatLong(apiEndPoint + zipCode + apiKey);
+    console.log(response);
 });

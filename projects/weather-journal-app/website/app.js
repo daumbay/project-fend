be1a011b4bb4c3642420ceb607baa5116545508a
Aditya Matsa <aditya.matsa@icloud.com>
2024-05-21 15:31:28 +0530

    Implement promise chain to get weather data

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -4,10 +4,10 @@
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-const apiEndPoint = 'https://api.openweathermap.org/geo/1.0/zip?zip=';
+const baseLatLon = 'https://api.openweathermap.org/geo/1.0/zip?zip=';
 const apiKey = '&appid=aac1d7f7625ee92bc0a51d78900a24a7';
 
-async function getLatLong (url = '') {
+async function getData (url = '') {
     const response = await fetch(url);
 
     try {
@@ -18,10 +18,18 @@ async function getLatLong (url = '') {
     }
 }
 
+function latlon (location) {
+    const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}${apiKey}`;
+    return getData(urlWeather);
+}
+
+function weatherData (weather) {
+    console.log(weather);
+}
+
 document.getElementById('generate').addEventListener('click', () => {
     const zipCode = document.getElementById('zip').value;
-    const response = getLatLong(apiEndPoint + zipCode + apiKey);
-    response.then((data) => {
-        console.log(data);
-    });
+    const response = getData(baseLatLon + zipCode + apiKey);
+    response.then(latlon).then(weatherData);
 });
+    
